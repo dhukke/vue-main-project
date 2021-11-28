c02de6059f461ff5a0c6fa80c89f618268184884
dhukke <dhukke@gmail.com>
2021-11-28 19:07:31 -0300

    first commit

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Register coach</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm },
+};
+</script>
